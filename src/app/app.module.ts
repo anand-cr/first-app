e82f3c1b6b74fdb41892716807c80521b5bf96ff
Anand_G99 <anand@detecttechnologies.com>
2022-10-04 13:07:41 +0530

    Creating new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // we add this so angular knows about its existence and
+    //  make sure it is imported so type script knows where to find it
+    ServerComponent 
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], 
+  bootstrap: [AppComponent] //which component when we start
 })
 export class AppModule { }
